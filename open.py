9f3fb5eeaef7d499b7e014160a3a320eea583cb2
panda2134 <ljypanda@live.com>
2021-09-05 12:46:42 +0800

    feat: Py3 support with six

--- open.py
+++ open.py
@@ -1,29 +1,30 @@
 # cross-platform version of: os.startfile()
 
 import os, sys, subprocess
+from six import string_types
 
-def start(input, application=''):
+def start(input_str, application=''):
 
-	# force input to be a string
-	if not isinstance(input, basestring):
-		input = ''
+	# force input_str to be a string
+	if not isinstance(input_str, string_types):
+		input_str = ''
 
 	# force application to be a string
-	if not isinstance(application, basestring):
+	if not isinstance(application, string_types):
 		application = ''
 
-	# open input with either default application or specified application
+	# open input_str with either default application or specified application
 	if sys.platform == 'darwin':
 		if application == '':
-			subprocess.call(['open', input])
+			subprocess.call(['open', input_str])
 		else:
-			subprocess.call(['open', '-a', application, input])
+			subprocess.call(['open', '-a', application, input_str])
 	elif sys.platform == 'win32':
 		if application == '':
-			subprocess.call(['start', '', input])
+			subprocess.call(['start', '', input_str])
 		else:
-			subprocess.call(['start', '', application, input])
+			subprocess.call(['start', '', application, input_str])
 	else:
 		if application == '':
 			application = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'vender', 'xdg-open')
-		subprocess.call([application, input])
+		subprocess.call([application, input_str])
